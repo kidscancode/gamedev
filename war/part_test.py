7f890de05831a9674194e95901d5e0497a9a136c
Chris Bradfield <cb@scribe.net>
2015-03-02 18:02:58 -0800

    Add l/r thrust particles

--- war/part_test.py
+++ war/part_test.py
@@ -13,9 +13,10 @@ OFFSETX = int(WIDTH / 2)
 OFFSETY = int(HEIGHT / 2)
 
 class Box(pygame.sprite.Sprite):
-    def __init__(self, image, *groups):
+    def __init__(self, game, image, *groups):
         pygame.sprite.Sprite.__init__(self, *groups)
         self.image = image
+        self.game = game
         self.base_image = self.image.copy()
         self.rect = self.image.get_rect()
         self.pos = pygame.math.Vector2(0, 0)
@@ -25,12 +26,15 @@ class Box(pygame.sprite.Sprite):
 
     def update(self):
         self.thrust = 0
+        self.act_thrust = 0
         self.rot = self.rot % 360
         keystate = pygame.key.get_pressed()
         if keystate[pygame.K_LEFT]:
             self.rot += 3
+            self.act_thrust = 3
         if keystate[pygame.K_RIGHT]:
             self.rot -= 3
+            self.act_thrust = -3
         if keystate[pygame.K_UP]:
             self.thrust = 1
 
@@ -55,12 +59,19 @@ class Game:
         self.bg_img = pygame.image.load('img/scrolling2.png').convert_alpha()
 
         self.all_sprites = pygame.sprite.Group()
-        self.box = Box(self.box_img, self.all_sprites)
+        self.box = Box(self, self.box_img, self.all_sprites)
         self.OFFSET = OFFSET
         em_offset = pygame.math.Vector2(0, 15)
         part_vel = pygame.math.Vector2(0, 3)
         self.emitter = ParticleEmitter(self, self.box, em_offset, part_vel,
                                        self.part_img, 0, 1, 0, 35, 10)
+        act_em_offset_r = pygame.math.Vector2(23, 6)
+        act_em_offset_l = pygame.math.Vector2(-23, 6)
+        act_part_vel = pygame.math.Vector2(0, 2)
+        self.act_emitter_r = ParticleEmitter(self, self.box, act_em_offset_r, act_part_vel,
+                                             self.part_img, 0, 0.2, 0, 15, 5)
+        self.act_emitter_l = ParticleEmitter(self, self.box, act_em_offset_l, act_part_vel,
+                                             self.part_img, 0, 0.2, 0, 15, 5)
 
     def run(self):
         running = True
@@ -81,11 +92,19 @@ class Game:
 
     def update(self):
         self.emitter.count = 0
+        self.act_emitter_r.count = 0
+        self.act_emitter_l.count = 0
         if self.box.thrust:
             self.emitter.count = 100
+        if self.box.act_thrust > 0:
+            self.act_emitter_r.count = 25
+        if self.box.act_thrust < 0:
+            self.act_emitter_l.count = 25
         self.all_sprites.update()
         self.emitter.update()
-        self.emitter.print_state()
+        self.act_emitter_r.update()
+        self.act_emitter_l.update()
+        # self.emitter.print_state()
 
     def draw(self):
         fps_txt = "FPS: {:.2f}  dt: {:.3f}  rot: {:.2f}".format(self.clock.get_fps(), self.dt, self.box.rot)
@@ -94,7 +113,13 @@ class Game:
         self.screen.blit(self.bg_img, [0, 0])
         self.all_sprites.draw(self.screen)
         self.emitter.draw()
+        self.act_emitter_r.draw()
+        self.act_emitter_l.draw()
         # pygame.draw.circle(self.screen, (0, 255, 0), (int(self.emitter.pos.x), int(self.emitter.pos.y)), 5)
+        # pygame.draw.circle(self.screen, (0, 255, 0), (int(self.act_emitter_r.pos.x),
+        #                                               int(self.act_emitter_r.pos.y)), 3)
+        # pygame.draw.circle(self.screen, (0, 255, 0), (int(self.act_emitter_l.pos.x),
+        #                                               int(self.act_emitter_l.pos.y)), 3)
         pygame.display.update()
 
 
