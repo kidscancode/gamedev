c939ccd2c3bba7f2bda46af51320eaaeaa31d7f8
Chris Bradfield <chris@kidscancode.org>
2015-10-04 22:29:07 -0700

    edits

--- tutorials/1-2 sprite example.py
+++ tutorials/1-2 sprite example.py
@@ -1,9 +1,10 @@
-# Pygame template - skeleton for a new pygame project
+# Pygame sprite Example
 import pygame
 import random
+import os
 
-WIDTH = 360
-HEIGHT = 480
+WIDTH = 800
+HEIGHT = 600
 FPS = 30
 
 # define colors
@@ -13,6 +14,21 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+class Player(pygame.sprite.Sprite):
+    # sprite for the Player
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.y_speed = 5
+
+    def update(self):
+        self.rect.x += 5
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+
 # initialize pygame and create window
 pygame.init()
 pygame.mixer.init()
@@ -21,6 +37,8 @@ pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 # Game loop
 running = True
 while running:
@@ -36,7 +54,7 @@ while running:
     all_sprites.update()
 
     # Draw / render
-    screen.fill((random.randrange(256), random.randrange(256), random.randrange(256)))
+    screen.fill(BLACK)
     all_sprites.draw(screen)
     # *after* drawing everything, flip the display
     pygame.display.flip()
